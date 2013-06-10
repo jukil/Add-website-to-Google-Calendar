700b62ece1009ea0983e840306c04f09a87d4920
Robert Orzanna <orschiro@gmail.com>
2013-06-10 17:36:07 +0200

    Initial release 1.0

---
+++ js/background.js
@@ -0,0 +1,138 @@
+
+//////////////////////////////////////////////////////////////////////
+//
+// This background page:
+//
+//   1) Holds the selected options and provides them
+//      to the other scripts when requested
+//
+//   2) Registers and handles the right-click context menus
+//
+//   3) Receives and handles Ctrl-Shift-M notifications from the 
+//      content scripts
+//
+//////////////////////////////////////////////////////////////////////
+
+var baseGmailUrl = "https://calendar.google.com/";
+var mailtoUrlSuffix = "mail/?extsrc=mailto&url=";
+var knownPorts = [];
+
+function makeGmailDomainUrl() {
+  var gmailUrl = baseGmailUrl;
+  var domainName = window.localStorage["domainName"];
+  if ((typeof domainName != "undefined") && (domainName != "")) {
+    gmailUrl += "a/" + domainName + "/";
+  }
+  return gmailUrl;
+}
+
+// Send message to the mailto script to update its cached gmail url and other options.
+// We also use it to add or remove the context menus
+// Note: the options page sends this on save
+chrome.extension.onConnect.addListener(function(port) {
+  if (port.name != "GmailUrlConn")
+    return;
+
+  port.onMessage.addListener(function(msg) {
+    if (msg.req == "OptionsPlease") {
+      port.postMessage({
+        gmailDomainUrl: makeGmailDomainUrl() + mailtoUrlSuffix,
+        windowOptions: window.localStorage["gmail_window_options"],
+        enableShortcut: !(window.localStorage["enable_shortcut"] == "off")
+      });
+      addToKnownPorts(port);
+    } else if (msg.req == "OptionsChanged") {
+      sendToKnownPorts({
+        gmailDomainUrl: makeGmailDomainUrl() + mailtoUrlSuffix,
+        windowOptions: window.localStorage["gmail_window_options"],
+        enableShortcut: !(window.localStorage["enable_shortcut"] == "off")
+      });
+      refreshContextMenus();
+    } else if (msg.req == "EmailThisPage") {
+      console.log(port);
+      openGmail(port.sender.tab.url, port.sender.tab.title);
+    } else {
+      console.log("Unsupported req on valid port");
+    }
+  });
+});
+
+function addToKnownPorts(port) {
+  knownPorts.push(port);
+  port.onDisconnect.addListener(function (p) {
+    var i = knownPorts.indexOf(p);
+    if (i == -1)
+      console.log("What? Disconnected from an unknown port?");
+    else knownPorts.splice(i,1);
+  });
+}
+
+function sendToKnownPorts(msg) {
+  knownPorts.forEach(function(p) { p.postMessage(msg); });
+}
+
+
+
+// Context menu callbacks
+function menuClicked(type, info, tab) {
+  var emailStr = "";
+  var emailSubj = "";
+  var addPage = true;
+  switch (type) {
+  case 'page':
+    emailStr = info.pageUrl;
+    emailSubj = tab.title;
+    addPage = false;
+    break;
+  case 'selection':
+    emailStr = info.selectionText;
+    emailSubj = tab.title;
+    break;
+  case 'link':
+    emailStr = info.linkUrl;
+    emailSubj = "A nice link";
+    break;
+  default:
+    emailStr = info.srcUrl;
+    emailSubj = "A nice " + type;
+  }
+  if (addPage)
+    emailStr = emailStr + "\nFrom: " + info.pageUrl;
+  openGmail(emailStr, emailSubj);
+}
+function openGmail(emailStr, emailSubj) {
+  // Build the Gmail URL
+  var gmailFullUrl = makeGmailDomainUrl() + "?action=TEMPLATE&trp=false" + "&text=" + encodeURIComponent(emailSubj) + "&details=" + encodeURIComponent(emailStr);
+
+  // Open the composition window/tab
+  window.open(gmailFullUrl,"_blank",window.localStorage["gmail_window_options"]);
+}
+function creationCallback() {
+  if (chrome.extension.lastError)
+    console.log("Error creating context menu: ", chrome.extension.lastError);
+}
+
+
+// Right-click menus registration
+function refreshContextMenus() {
+  chrome.contextMenus.removeAll();
+  if (!(window.localStorage["enable_email_this"] == "off"))
+    contextMenusAdd();
+}
+
+function contextMenusAdd() {
+  chrome.contextMenus.create({ "title": "&Add to Google Calendar", "contexts": ['page'], 
+    "onclick": function(info,tab) { menuClicked('page',info,tab) } }, creationCallback);
+  chrome.contextMenus.create({ "title": "&Add to Google Calendar", "contexts": ['selection'], 
+    "onclick": function(info,tab) { menuClicked('selection',info,tab) } }, creationCallback);
+  chrome.contextMenus.create({ "title": "&Add to Google Calendar", "contexts": ['link'], 
+    "onclick": function(info,tab) { menuClicked('link',info,tab) } }, creationCallback);
+  chrome.contextMenus.create({ "title": "&Add to Google Calendar", "contexts": ['image'], 
+    "onclick": function(info,tab) { menuClicked('image',info,tab) } }, creationCallback);
+  chrome.contextMenus.create({ "title": "&Add to Google Calendar", "contexts": ['video'], 
+    "onclick": function(info,tab) { menuClicked('video',info,tab) } }, creationCallback);
+  chrome.contextMenus.create({ "title": "&Add to Google Calendar", "contexts": ['audio'], 
+    "onclick": function(info,tab) { menuClicked('audio clip',info,tab) } }, creationCallback);
+}
+
+refreshContextMenus();
---
+++ js/mailto.js
@@ -0,0 +1,76 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 Jacopo Corbetta. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var cachedGmailUrl = "";
+var windowOptions = "";
+var listenersAdded = false;
+var enableShortcut = false;
+
+function encodeForMailto(inUrl) {
+  // GMail unescapes most of the string in the first step,
+  // so "%2B" would get replaced with "+". Unfortunately,
+  // the next step still tries to decode the string, and
+  // plus is interpreted as space.
+  // The same is true for "#" (interpreted as start of fragment)
+  // Workaround: double encoding for + and #.
+  inUrl = inUrl.replace(/\+/g,"%2B", "g")
+  inUrl = inUrl.replace(/#/g,"%23", "g")
+  return encodeURIComponent(inUrl);
+}
+
+function registerDocumentListener() {
+  document.addEventListener("click", function(ev) {
+    var nod = ev.target;
+    while (nod) {
+      if (nod.href && (nod.href.substr(0,7) == "mailto:")) {
+  		loadJsFile(chrome.extension.getURL("js/gan.js"));
+        window.open(cachedGmailUrl + encodeForMailto(nod.href), "_blank", windowOptions);
+        ev.preventDefault();
+        ev.stopPropagation();
+      }
+      if (nod.href)
+	  break;
+      nod = nod.parentNode;
+    }
+  }, false);
+
+  listenersAdded = true;
+}
+
+function keyupListener(ev)
+{
+  if (ev.ctrlKey && ev.shiftKey && (ev.keyCode == 77))
+    bgPort.postMessage({req: "EmailThisPage"});
+}
+
+
+if (cachedGmailUrl != "") {
+  registerDocumentListener();
+}
+
+var bgPort = chrome.extension.connect({name: "GmailUrlConn"});
+bgPort.postMessage({req: "OptionsPlease"});
+bgPort.onMessage.addListener(
+function(msg) {
+  //console.log("Got message from bg page - " + msg.windowOptions);
+  cachedGmailUrl = msg.gmailDomainUrl;
+  windowOptions = msg.windowOptions;
+  enableShortcut = msg.enableShortcut;
+
+  if (!listenersAdded)
+    registerDocumentListener();
+
+  if (enableShortcut)
+    window.addEventListener("keyup", keyupListener, false);
+  else window.removeEventListener("keyup", keyupListener, false);
+});
+
+
+function loadJsFile(filename){
+	var fileref=document.createElement('script');
+	fileref.setAttribute("type","text/javascript");
+	fileref.setAttribute("src", filename);
+	document.getElementsByTagName("head")[0].appendChild(fileref);
+}
